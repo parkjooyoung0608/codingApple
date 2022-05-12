a900ed87bd441426daf02a62e120b30fa115a0e2
jooyoung <mirkduddl@naver.com>
2022-05-12 14:50:06 +0900

    숙제: list를 3개로 늘리고 제목들을 state로 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  let [a] = useState('강남 우동 맛집');
+  let [b] = useState('강남 우동 맛집');
+  let [c] = useState('강남 우동 맛집');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='black-nav'>
+            <h4>블로그임</h4>
+        </div>
+        <div className='list'>
+          <h4>{a}</h4>
+          <p>2월 17일 발행</p>
+        </div>
+        <div className='list'>
+          <h4>{b}</h4>
+          <p>2월 17일 발행</p>
+        </div>
+        <div className='list'>
+          <h4>{c}</h4>
+          <p>2월 17일 발행</p>
+        </div>
     </div>
   );
 }
