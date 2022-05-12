01a1ca84cdf76514fa701e377287bfe4bfe6c2eb
jooyoung <mirkduddl@naver.com>
2022-05-12 21:52:50 +0900

    ADD: state로 제목 클릭 시 modal 창을 띄우는 조건문 구현

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { useState } from 'react';
 
 function App() {
   
-  let [a] = useState('강남 우동 맛집');
-  let [b] = useState('강남 우동 맛집');
-  let [c] = useState('강남 우동 맛집');
+  let [글제목, 글제목변경] = useState(['남자코트 추천','강남 우동 맛집','파이썬독학','여자 코드 추천'])
+  let [따봉, 따봉변경] = useState(0)
+  let [modal, setModal] = useState(false)
+  
 
   return (
     <div className="App">
@@ -13,19 +14,38 @@ function App() {
             <h4>블로그임</h4>
         </div>
         <div className='list'>
-          <h4>{a}</h4>
+          <h4>{ 글제목[0] }<span onClick={()=>{따봉변경(따봉 + 1)}}>💛</span> {따봉} </h4>
+          <p>2월 17일 발행</p>
+          <button onClick={ () => {
+              let copy = [...글제목];
+              글제목변경(copy.sort());
+            }} >글 제목 바꾸기</button>
+        </div>
+        <div className='list'>
+          <h4>{ 글제목[1] }</h4>
           <p>2월 17일 발행</p>
         </div>
         <div className='list'>
-          <h4>{b}</h4>
+          <h4 onClick={ ()=>{setModal(!modal)} }>{ 글제목[2] }</h4>
           <p>2월 17일 발행</p>
         </div>
-        <div className='list'>
-          <h4>{c}</h4>
-          <p>2월 17일 발행</p>
-        </div>
+        
+        {
+          modal === true ? <Modal></Modal> : null  
+        }
+        
     </div>
   );
+}
+
+function Modal(){
+  return (
+        <div className="modal">
+          <h4>제목</h4>
+          <p>날짜</p>
+          <p>상세내용</p>
+        </div>
+  )
 }
 
 export default App;
